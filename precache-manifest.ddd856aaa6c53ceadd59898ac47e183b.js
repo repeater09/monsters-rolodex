8ce81da6858c5bc3ea1c21dce8c3c660fe1ff58e
Robert Hurd <repeater09@gmail.com>
2020-09-28 16:57:56 -0500

    Updates

---
+++ precache-manifest.ddd856aaa6c53ceadd59898ac47e183b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fea3a5a5c425aba047ec42fd3ab63ac0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0d365ce57840cec7ea0c",
+    "url": "/monsters-rolodex/static/css/main.5e7335da.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d365ce57840cec7ea0c",
+    "url": "/monsters-rolodex/static/js/main.3e1873ed.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
